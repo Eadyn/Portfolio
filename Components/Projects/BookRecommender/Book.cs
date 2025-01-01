69f8bd657e8aa0e672744b176ddf5f306641c0fb
Eadyn <thompsone1150@gmail.com>
2025-01-01 13:36:13 -0500

    Added functionality for searching by book description instead of just finding similar books to ones in our database, hopefully much better overall.

---
+++ Components/Projects/BookRecommender/Book.cs
@@ -0,0 +1,10 @@
+namespace Books
+{
+public class BookResult
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string CoverImg { get; set; }
+        public double Rating { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddRazorComponents()
 
 var baseAddress = builder.Environment.IsDevelopment() ? "https://localhost:5046" : "https://eadynt.com";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-builder.Services.AddScoped<BookService>();
+// builder.Services.AddScoped<BookService>();
 
 var app = builder.Build();
 
